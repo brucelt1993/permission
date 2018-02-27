ba8ea04ccf274384d786bab8aaad8a4ef5d4f621
brucelt1993 <brucelt_1993@163.com>
2018-02-27 23:45:11 +0800

    Initial commit

---
+++ src/main/java/com/mmall/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.mmall.controller;
+
+import com.mmall.common.JsonData;
+import com.mmall.exception.PermissionException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+    @RequestMapping("/hello.json")
+    @ResponseBody
+    public JsonData hello(){
+        log.info("hello");
+        return JsonData.success("hello, permission");
+    }
+    @RequestMapping("/hello.exception")
+    @ResponseBody
+    public JsonData helloException(){
+        log.info("hello");
+        throw new PermissionException("msg");
+    }
+}
