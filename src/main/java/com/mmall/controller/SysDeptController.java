a7d0773d19c89a6dddac1073336d0954a77357bb
brucelt1993 <brucelt_1993@163.com>
2018-03-01 23:41:44 +0800

    commit by bruce 2018-03-01

---
+++ src/main/java/com/mmall/controller/SysDeptController.java
@@ -0,0 +1,51 @@
+package com.mmall.controller;
+
+import com.mmall.common.JsonData;
+import com.mmall.dto.DeptLevelDto;
+import com.mmall.param.DeptParam;
+import com.mmall.service.SysDeptService;
+import com.mmall.service.SysTreeSevice;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping("/sys/dept")
+@Slf4j
+public class SysDeptController {
+    @Resource
+    private SysDeptService sysDeptService;
+    @Resource
+    private SysTreeSevice sysTreeSevice;
+
+    @RequestMapping("/dept.page")
+    public ModelAndView page() {
+        return new ModelAndView("dept");
+    }
+
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveDept(DeptParam param) {
+        sysDeptService.save(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/tree.json")
+    @ResponseBody
+    public JsonData tree() {
+        List<DeptLevelDto> dtoList = sysTreeSevice.deptTree();
+        return JsonData.success(dtoList);
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateDept(DeptParam param) {
+        sysDeptService.update(param);
+        return JsonData.success();
+    }
+}
