92418b62ed820d62cb7b9de9d953e591f669ef59
brucelt1993 <brucelt_1993@163.com>
2018-03-06 23:39:25 +0800

    commit by bruce 2018-03-06

--- src/main/java/com/mmall/controller/SysAclController.java
+++ src/main/java/com/mmall/controller/SysAclController.java
@@ -1,4 +1,40 @@
 package com.mmall.controller;
 
+import com.mmall.beans.PageQuery;
+import com.mmall.common.JsonData;
+import com.mmall.param.AclParam;
+import com.mmall.service.SysAclService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/sys/acl")
+@Slf4j
 public class SysAclController {
+    @Resource
+    private SysAclService sysAclService;
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveAcl(AclParam param) {
+        sysAclService.save(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateAcl(AclParam param) {
+        sysAclService.update(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/page.json")
+    @ResponseBody
+    public JsonData list(@RequestParam("aclModuleId") Integer aclModuleId, PageQuery pageQuery) {
+        return JsonData.success(sysAclService.getPageByAclModuleId(aclModuleId, pageQuery));
+    }
 }
--- src/main/java/com/mmall/controller/SysAclModuleController.java
+++ src/main/java/com/mmall/controller/SysAclModuleController.java
@@ -1,8 +1,10 @@
 package com.mmall.controller;
 
 import com.mmall.common.JsonData;
+import com.mmall.dto.AclModuleLevelDto;
 import com.mmall.param.AclModuleParam;
 import com.mmall.service.SysAclModuleService;
+import com.mmall.service.SysTreeSevice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
 @RequestMapping("/sys/aclModule")
@@ -17,6 +20,8 @@ import javax.annotation.Resource;
 public class SysAclModuleController {
     @Resource
     private SysAclModuleService sysAclModuleService;
+    @Resource
+    private SysTreeSevice sysTreeSevice;
     @RequestMapping("/acl.page")
     public ModelAndView page() {
         return new ModelAndView("acl");
@@ -42,5 +47,17 @@ public class SysAclModuleController {
     public JsonData updateAclModule(AclModuleParam param) {
         sysAclModuleService.update(param);
         return JsonData.success();
+    }
+
+    /**
+     * 获取权限树
+     *
+     * @return
+     */
+    @RequestMapping("/tree.json")
+    @ResponseBody
+    public JsonData tree() {
+        List<AclModuleLevelDto> dtoList = sysTreeSevice.aclModuleTree();
+        return JsonData.success(dtoList);
     }
 }
